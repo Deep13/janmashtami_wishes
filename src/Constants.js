161dafeefa69ea3e3ecb63910dfd027a61950ca9
Deep13 <deep.deepak30@gmail.com>
2023-03-08 20:29:51 +0530

    first commit

---
+++ src/Constants.js
@@ -0,0 +1,117 @@
+export const day_title = 'Happy Janmashtami';
+export const banner_key1 = 'ca-app-pub-4717579333914549/7763917095';
+export const banner_key2 = 'ca-app-pub-4717579333914549/7549484048';
+export const banner_key3 = 'ca-app-pub-4717579333914549/5779191082';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/3153027744';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/1839946076';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/9253092736';
+export const language = [
+  {id: 'en', lang: 'English'},
+  {id: 'hi', lang: 'Hindi'},
+];
+export const textData = {
+  English: [
+    {
+      id: '001',
+      text:
+        'May Lord Krishna steal all your worries and give you peace, and happiness on this holy occasion of Krishna Janmashtami!',
+    },
+    {
+      id: '002',
+      text:
+        'On this Janmashtami, may Lord Krishna fill your home with lots of happiness and joy. Happy Janmashtami to you and your loved ones!',
+    },
+    {
+      id: '003',
+      text:
+        'May the blessings of Lord Krishna always be with you and your family. Wishing you and your family a very a happy Janmashtami!',
+    },
+    {
+      id: '004',
+      text:
+        'On this Janmashtami, may all your wishes come true and may Nand Gopal shower his blessings on you and your loved ones! Happy Janmashtami!',
+    },
+    {
+      id: '005',
+      text:
+        'May Krishna ji always bless you with happiness, love and peace. Happy Janmashtami to you and your family!',
+    },
+    {
+      id: '006',
+      text:
+        'Leave all your worries to Lord Krishna on this day and he will take care of you. Happy Janmashtami!',
+    },
+    {
+      id: '007',
+      text:
+        'May your life be filled with love, happiness, laughter and Krishna’s blessings. Happy Janmashtami!',
+    },
+    {
+      id: '008',
+      text:
+        'This Janmashtami, I wish Krishna’s blissful tunes fill your life with bliss and joy. Happy Janmashtami!',
+    },
+    {
+      id: '009',
+      text:
+        'May Krishna fill your home and heart with love, joy, good health and happiness. Happy Janmashtami!',
+    },
+    {
+      id: '010',
+      text:
+        'Jai Shri Krishna! Have a happy Janmashtami. I pray to Krishna today to take away all your problems and worries. Radhe Radhe!',
+    },
+  ],
+  Hindi: [
+    {
+      id: '001',
+      text:
+        'राधा की भक्ति , मुरली की मिठास , माखन का स्वाद और गोपियों का रास , आओ सब मिलके बनाते हैं जन्माष्टमी का दिन ख़ास। श्रीकृष्ण जन्‍माष्‍टमी की हार्दिक शुभकामनाएं.',
+    },
+    {
+      id: '002',
+      text:
+        'माखन चुराकर जिसने खाया , बंसी बजाकर जिसने नचाया , ख़ुशी मनाओ उनके जन्मदिन की , जिसने दुनिया को प्रेम का रास्ता दिखाया। श्रीकृष्ण जन्‍माष्‍टमी की हार्दिक शुभकामनाएं..',
+    },
+    {
+      id: '003',
+      text:
+        'माखन का कटोरा , मिश्री का थाल , मिट्टी की खुशबू , बारिश की फुहार , राधा की उम्मीद कन्हिया का प्यार , मुबारक हो आपको जन्माष्टमी का त्यौहार श्रीकृष्ण जन्‍माष्‍टमी की हार्दिक शुभकामनाएं..',
+    },
+    {
+      id: '004',
+      text:
+        'माखन -चोर नन्द -किशोर , बाँधी जिसने प्रीत की डोर , हरे कृष्णा हरे मुरारी , पूजती जिन्हें दुनिया सारी , आओ उनके गुण गायें , सब मिलके जन्माष्टमी मनाएँ श्रीकृष्ण जन्‍माष्‍टमी की हार्दिक शुभकामनाएं..',
+    },
+    {
+      id: '005',
+      text:
+        'जो है माखन चोर, जो है मुरली वाला, वही है हम सबके दुःख दूर करने वाला।। Happy Janmashtami..',
+    },
+    {
+      id: '006',
+      text:
+        'जय हो मुरली धर गोपाल की, जय हो कन्हैया लाल की। Happy Janmashtami..',
+    },
+    {
+      id: '007',
+      text:
+        'हरे कृष्ण हरे मुरारी, पुजती जिन्हें दुनिया सारी, आओ उनके गुण गाएं सब मिल के जन्माष्टमी मनाये! Happy Shree Krishna Janmashtami..',
+    },
+    {
+      id: '008',
+      text:
+        'कर भरोसा राधे नाम का धोखा कभी न खायेगा, हर मौके पर कृष्ण तेरे घर सबसे पहले आयेगा..।। Happy Krishna Janmashtami..',
+    },
+    {
+      id: '009',
+      text:
+        'कृष्ण जिनका नाम गोकुल जिनका धाम है ऐसे श्री कृष्ण भगवन को हम सब का प्रणामHappy Janmashtami..',
+    },
+    {
+      id: '010',
+      text:
+        'इस जन्माष्टमी पर श्री कृष्णा आपके घर आये और माखन मिश्री के साथ सरे दुःख और कष्ट भी ले जाए..',
+    },
+  ],
+};
